f502791f485ade671f08fca6120714d5e04e141d
Selefin <melvynakesse@gmail.com>
2025-10-10 09:00:44 +0200

    Smooth closing when using /disconnect command

--- bot.py
+++ bot.py
@@ -46,43 +46,10 @@ async def on_connect():
 
 @bot.event
 async def on_disconnect():
+    print("Closing HTTP session...")
+    if hasattr(bot, 'http') and hasattr(bot.http, '_HTTPClient__session'):
+            await bot.http._HTTPClient__session.close()
     print("Bot disconnected from discord")
-
-
-@bot.listen()
-async def on_member_join(member):
-    file_path = f'./roles/{member.guild.id}_roles.json'
-    if not os.path.exists(file_path):
-        return
-    with open(file_path, 'r') as f:
-        guild_roles = json.load(f)
-    if str(member.id) not in guild_roles:
-        return
-    roles = [member.guild.get_role(role_id) for role_id in guild_roles[str(member.id)]]
-    await member.add_roles(*roles)
-    print(f"Roles have been added to {member.name} in {member.guild.name}")
-
-@bot.listen()
-async def on_ready():
-    if not os.path.exists('./roles'):
-        os.makedirs('./roles')
-    for guild in bot.guilds:
-        members = guild.fetch_members()
-        if members is None:
-            print(f"Failed to fetch members in {guild.name}")
-            continue
-        guild_roles = {}
-        while True:
-            try:
-                member = await members.__anext__()
-                member_roles = [role.id for role in member.roles if role.name != "@everyone"]
-                guild_roles[member.id] = member_roles
-            except StopAsyncIteration:
-                break
-        file_path = f'./roles/{guild.id}_roles.json'
-        with open(file_path, 'w') as f:
-            json.dump(guild_roles, f, indent=4)
-        print(f"Roles for guild {guild.name} have been saved to {file_path}")
 
 ## Commands
 @bot.slash_command(name="commands", description="List all available commands")
@@ -106,8 +73,17 @@ async def disconnect(ctx):
         return
     await ctx.respond("Disconnecting...")
     print("Disconnecting...")
-    await bot.close()
-    exit(0)
+    
+    try:
+        for voice_client in bot.voice_clients:
+            if voice_client.is_connected():
+                await voice_client.disconnect(force=True)
+
+        await bot.close()
+        print("Bot disconnected successfully.")
+    except Exception as e:
+        print(f"Error during disconnect: {e}")
+    sys.exit(0)
 
 @bot.slash_command(name="restart", description="Restart the bot")
 async def restart(ctx):
